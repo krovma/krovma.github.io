713cbba18edc25c7a25c75c31d431dbe4c5d060a
Krovma <krovma726@gmail.com>
2021-02-13 02:09:45 +0800

    test text update

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,14 +22,15 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/')
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Patreon', 'https://www.patreon.com/krovma726'),
+          ('EOS: kvma2mtfkvma(no memo)', '#'),
+          ('BTC: 3QfVY7SReQu5Jof1nSmrCDBdoxs6Ec1kmb', '#'))
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
